3364d25312f512cc0274e48c4ff6914414f3e167
Philip de Nier <philipn@rd.bbc.co.uk>
2023-02-15 11:30:16 +0000

    Constrain git version to libMXF

--- cmake/git_version.cmake
+++ cmake/git_version.cmake
@@ -3,6 +3,9 @@ if(TARGET libmxf_git_version)
 endif()
 
 
+# Ensure the cmake targets and code symbols are libMXF specific
+set(GIT_VERSION_PROJECT_PREFIX "libmxf_")
+
 # Set the git release tag pattern to describe relative to
 set(GIT_DESCRIBE_TAG_PATTERN "v${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
 set(GIT_WORKING_DIR ${PROJECT_SOURCE_DIR})
